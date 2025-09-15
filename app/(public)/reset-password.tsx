7b321ac2dc6b9dc47054725e769df3e7f4eb3126
Morten Muusmann <morten.muusmann@gmail.com>
2025-09-15 09:35:02 +0200

    CI: lint/typecheck/tests + web build

---
+++ app/(public)/reset-password.tsx
@@ -0,0 +1,6 @@
+// app/(public)/reset-password.tsx
+export { default } from "./reset-password.web";
+
+// (valgfrit) hvis du havde 'export const options = {...}' i web-filen,
+// kan du også gen-eksportere den:
+// export { options } from "./reset-password.web";
